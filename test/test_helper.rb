bcf23c2bb7b9564df28918ba606d473a801b036f
Jessica Owens <vertige@gmail.com>
2017-10-18 09:52:35 -0700

    initial setup for mocking OAuth testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -22,5 +22,20 @@ Minitest::Reporters.use!(
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+
+  def setup
+    OmniAuth.config.test_mode = true
+  end
+
+  def mock_auth_hash(user)
+    return {
+      provider: user.provider,
+      uid: user.uid,
+      info: {
+        email: user.email,
+        nickname: user.username
+      }
+    }
+  end
   # Add more helper methods to be used by all tests here...
 end
